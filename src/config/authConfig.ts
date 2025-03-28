2b73a195c6277d0fb54a4e7b62750b4e350aa85c
gasgustavo <gustavo.pitangui@gmail.com>
2025-03-28 10:29:32 -0300

    using correct variable

--- src/config/authConfig.ts
+++ src/config/authConfig.ts
@@ -1,6 +1,6 @@
 import { Configuration, LogLevel } from "@azure/msal-browser";
 
-const appUrl = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
+const appUrl = process.env.NEXT_PUBLIC_REDIRECT_URI || "http://localhost:3000";
 
 export const msalConfig: Configuration = {
     auth: {
